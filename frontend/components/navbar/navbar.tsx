74477f3248d5f9381c6e47732ed32802e1450a97
Jose Huerta <josehuerta115@gmail.com>
2023-03-29 22:14:33 -0700

    updates

--- frontend/components/navbar/navbar.tsx
+++ frontend/components/navbar/navbar.tsx
@@ -1,3 +1,30 @@
-export default function Home() {
-  return <div></div>;
+import styles from './navbar.module.css';
+import { Menu, Search } from '@mui/icons-material';
+import { useState } from 'react';
+
+export default function Navbar() {
+  const storeName = 'Hype Club';
+
+  const [menuActive, setMenuActive] = useState(false);
+
+  function handleMenuActive() {
+    setMenuActive((menuActive) => !menuActive);
+  }
+
+  return (
+    <nav className={`${styles['nav-container']}`}>
+      <button
+        className={`${styles['menu-button']}`}
+        onClick={() => {
+          console.log('clicked');
+        }}
+      >
+        <Menu sx={{ fontSize: '2rem', color: 'black' }}></Menu>
+      </button>
+      <div className={styles.branding}>{storeName}</div>
+      <button className={`${styles['search-button']}`}>
+        <Search sx={{ fontSize: '2rem', color: 'black' }}></Search>
+      </button>
+    </nav>
+  );
 }
