e2eb606057a70d25942f2fbb8e8f3aeec5f8e014
Tuti <josehuerta115@gmail.com>
2023-03-30 14:32:24 -0700

    more navbar work, css and html changes

--- frontend/components/navbar/navbar.tsx
+++ frontend/components/navbar/navbar.tsx
@@ -2,13 +2,22 @@ import styles from './navbar.module.css';
 import { Menu, Search, Close } from '@mui/icons-material';
 import { useState } from 'react';
 
-export default function Navbar() {
-  const storeName = 'Hype Club';
-  const activeMenuStyle = `${styles['menu']} ${styles['animate-menu']} ${styles['active-menu']}`;
+interface props {
+  storeName: string;
+}
+export default function Navbar(props: props) {
+  const activeMenuStyle = `${styles['menu']} ${styles['active-menu']}`;
+  const activeSearchStyle = `${styles['search-form']} ${styles['active-search-form']}`;
+
   const [menuActive, setMenuActive] = useState(false);
+  const [searchActive, setSearchActive] = useState(false);
 
   function handleMenuActive() {
     setMenuActive((menuActive) => !menuActive);
+  }
+
+  function handleSearchActive() {
+    setSearchActive((searchActive) => !searchActive);
   }
 
   return (
@@ -19,7 +28,10 @@ export default function Navbar() {
       <button
         title="menu"
         type="button"
-        className={`${styles['menu-button']}`}
+        className={styles['menu-button']}
+        // className={
+        //   searchActive ? styles['invisible'] : `${styles['menu-button']}`
+        // }
         onClick={handleMenuActive}
       >
         <Menu
@@ -31,13 +43,22 @@ export default function Navbar() {
           sx={{ fontSize: '2rem', color: 'black' }}
         />
       </button>
-      <div className={styles.branding}>{storeName}</div>
+      <div className={styles.branding}>{props.storeName}</div>
+      <form
+        className={
+          searchActive ? activeSearchStyle : `${styles['search-form']}`
+        }
+      >
+        <input className={styles['search-input']} placeholder="Search"></input>
+      </form>
       <button
         title="search-button"
         type="button"
         className={
           menuActive ? `${styles['invisible']}` : `${styles['search-button']}`
         }
+        onClick={handleSearchActive}
+        onBlur={handleSearchActive}
       >
         <Search sx={{ fontSize: '2rem', color: 'black' }} />
       </button>
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import Navbar from '@/components/navbar/navbar';
 import styles from '@/styles/Home.module.css';
 
+const storeName: string = 'Hype Club';
 export default function Home() {
   return (
     <>
@@ -11,7 +12,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Navbar></Navbar>
+      <Navbar storeName={storeName}></Navbar>
       <main>
         <div className={styles['landing-page-image']}></div>
       </main>
